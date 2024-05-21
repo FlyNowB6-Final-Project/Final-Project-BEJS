573c5fc09ac400b444c483af6eb02cfd23649daa
Bagus Dwi <sugab.dwi88@gmail.com>
2024-05-21 22:10:43 +0700

    First Commit

---
+++ controllers/user.controllers.js
@@ -0,0 +1,7 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = process.env;
+
+module.exports = {}
