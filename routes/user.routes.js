883fb16fd5dd397e0ed94bcc0974fd9c73cd89d8
Bagus Dwi <sugab.dwi88@gmail.com>
2024-05-22 23:43:23 +0700

    Auth API

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const { register, login, auth } = require("../controllers/user.controllers");
+const restrict = require("../middlewares/auth.middlewares");
+
+// API Auth
+router.post("/auth/register", register);
+router.post("/auth/login", login);
+router.get("/auth/authenticate", restrict, auth);
 
 module.exports = router;
