36c8809c99d41e5a0cc1a23450f969e6a46b0d80
Akhdanre <danakhdan12@gmail.com>
2024-05-23 22:19:02 +0700

    feat : rollback for country and city

--- prisma/seeder/roolback_seed.js
+++ prisma/seeder/roolback_seed.js
@@ -2,6 +2,8 @@ async function roolbackSeed(prisma) {
     try {
         await prisma.plane.deleteMany()
         await prisma.airlines.deleteMany()
+        await prisma.city.deleteMany()
+        await prisma.country.deleteMany()
         await prisma.continent.deleteMany()
         await prisma.$executeRaw`TRUNCATE TABLE flights, detail_plane, passengers, orders, detail_flight, planes, airlines, cities, countries, continents RESTART IDENTITY`
         console.log("success rollback all data")
