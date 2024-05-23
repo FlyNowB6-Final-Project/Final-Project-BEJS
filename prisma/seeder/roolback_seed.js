b4fc7cada398cce3a6fc451ec55e5f45f3f5f8a0
Akhdanre <danakhdan12@gmail.com>
2024-05-23 22:42:22 +0700

    feat : rollback for seat class

--- prisma/seeder/roolback_seed.js
+++ prisma/seeder/roolback_seed.js
@@ -5,6 +5,7 @@ async function roolbackSeed(prisma) {
         await prisma.city.deleteMany()
         await prisma.country.deleteMany()
         await prisma.continent.deleteMany()
+        await prisma.seatClass.deleteMany()
         await prisma.$executeRaw`TRUNCATE TABLE flights, detail_plane, passengers, orders, detail_flight, planes, airlines, cities, countries, continents RESTART IDENTITY`
         console.log("success rollback all data")
     } catch (e) {
