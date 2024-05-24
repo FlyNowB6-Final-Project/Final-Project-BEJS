e0f88ea4c7ab5029a2305a6adbb8e5e8f9a8d7e7
Akhdanre <danakhdan12@gmail.com>
2024-05-24 19:40:04 +0700

    fix : truncate on rollback

--- prisma/seeder/roolback_seed.js
+++ prisma/seeder/roolback_seed.js
@@ -6,7 +6,7 @@ async function roolbackSeed(prisma) {
         await prisma.country.deleteMany()
         await prisma.continent.deleteMany()
         await prisma.seatClass.deleteMany()
-        await prisma.$executeRaw`TRUNCATE TABLE flights, detail_plane, passengers, orders, detail_flight, planes, airlines, cities, countries, continents RESTART IDENTITY`
+        await prisma.$executeRaw`TRUNCATE TABLE seat_class, flights, detail_plane, passengers, orders, detail_flight, planes, airlines, cities, countries, continents RESTART IDENTITY`
         console.log("success rollback all data")
     } catch (e) {
         console.log(e)
