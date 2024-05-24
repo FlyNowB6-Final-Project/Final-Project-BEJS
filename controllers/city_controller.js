1b2be699e4d4a8a76f69aaa65828b74719be756f
Akhdanre <danakhdan12@gmail.com>
2024-05-24 18:15:45 +0700

    feat : find city

--- controllers/city_controller.js
+++ controllers/city_controller.js
@@ -1,7 +1,8 @@
 const cityService = require("../service/city_service")
 
 const getAllCity = async (req, res, next) => {
-    let data = await cityService.get();
+    let { find } = req.query
+    let data = await cityService.get(find);
     if (!data) {
         res.status(400).json({
             status: false,
--- service/city_service.js
+++ service/city_service.js
@@ -1,9 +1,19 @@
-const { PrismaClient } = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
+const { param } = require("../routes/city.routes");
 const prisma = new PrismaClient()
 
 const get = async (params) => {
     try {
-        return await prisma.city.findMany();
+        return await prisma.city.findMany(
+            {
+                where: {
+                    name: {
+                        contains: params
+                    }
+                }
+
+            }
+        );
     } catch (error) {
         throw error
     }
