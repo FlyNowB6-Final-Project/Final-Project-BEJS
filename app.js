c023c2d522855ca77599692f90da3b0c00cb975e
Akhdanre <danakhdan12@gmail.com>
2024-05-25 22:58:13 +0700

    fix : conflict

--- app.js
+++ app.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cors = require("cors");
-const routes = require("./routes");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
-app.use(routes);
-
-app.get("/", (req, res) => {
-  res.send(`<h1 align="center">Hello World</h1>`);
-});
-
-// 404 error handler
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: false,
-    message: `are you lost? ${req.method} ${req.url} is not registered!`,
-    data: null,
-  });
-});
-
-// 500 error handler
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({
-    status: false,
-    message: err.message,
-    data: null,
-  });
-});
-
-app.listen(PORT, () => console.log("Listening on port :", PORT));
-
-module.exports = app;
-=======
 require("dotenv").config();
 const express = require('express');
 const path = require('path');
@@ -99,4 +53,3 @@ app.listen(PORT, () => console.log("Listening on port :", PORT));
 // server.listen(PORT, () => console.log("Listening on port :", PORT));
 
 module.exports = app;
->>>>>>> develop
