1d24255c2a752b925f655d166ec3de4a02e0d23b
NoctXV <kuroshiro30243@gmail.com>
2024-05-29 08:55:48 +0700

    get & update profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { register, login, verifyOtp, resendOtp, forgetPassword, resetPassword, auth } = require("../controllers/user.controllers");
+const { register, login, verifyOtp, resendOtp, forgetPassword, resetPassword, getDetail, updateProfile, auth } = require("../controllers/user.controllers");
 const restrict = require("../middlewares/auth.middlewares");
 
 // API Auth Users
@@ -9,6 +9,8 @@ router.put("/users/verify-otp", verifyOtp);
 router.put("/users/resend-otp", resendOtp);
 router.post("/users/forget-password", forgetPassword);
 router.put("/users/reset-password", resetPassword);
+router.get('/users/:id', getDetail);
+router.put('/users/update-profile/:id', updateProfile);
 router.get("/users/authenticate", restrict, auth);
 
 module.exports = router;
