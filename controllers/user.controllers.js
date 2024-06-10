03fb30cb898e1fe8a796764099e4fe40a0c76ec0
Bagus Dwi <sugab.dwi88@gmail.com>
2024-06-10 22:28:59 +0700

    Fix Midtrans

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -292,10 +292,8 @@ module.exports = {
 
       const html = await nodemailer.getHTML("link-reset.ejs", {
         name: user.fullname,
-        // url: `${req.protocol}://${req.get(
-        //   "host"
-        // )}/api/v1/users/reset-password?token=${token}`,
-        url: `http://localhost:5173/reset-password?token=${token}`
+        url: `${req.protocol}://${req.get("host")}/reset-password?token=${token}`
+        // url: `http://localhost:5173/reset-password?token=${token}`
       });
 
       await nodemailer.sendMail(email, "Password Reset Request", html);
