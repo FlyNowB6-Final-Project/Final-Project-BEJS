94e6a3c056d40763d3ce6570e6049e7ffa6b9e5b
Akhdanre <danakhdan12@gmail.com>
2024-06-13 01:00:58 +0700

    feat : new assets

--- prisma/seeder/airlanes_seed.js
+++ prisma/seeder/airlanes_seed.js
@@ -1,78 +1,9 @@
-const data = [
-    {
-        code: "GA123",
-        name: "Garuda Indonesia",
-        since: "1949",
-        logo_url: "http://example.com/logos/garuda.png",
-
-    },
-    {
-        code: "LI234",
-        name: "Lion Air",
-        since: "1999",
-        logo_url: "http://example.com/logos/lionair.png",
-
-    },
-    {
-        code: "BA345",
-        name: "Batik Air",
-        since: "2013",
-        logo_url: "http://example.com/logos/batikair.png",
-
-    },
-    {
-        code: "CI456",
-        name: "Citilink",
-        since: "2001",
-        logo_url: "http://example.com/logos/citilink.png",
-
-    },
-    {
-        code: "AI567",
-        name: "AirAsia Indonesia",
-        since: "2004",
-        logo_url: "http://example.com/logos/airasia.png",
-
-    },
-    {
-        code: "SP678",
-        name: "Sriwijaya Air",
-        since: "2003",
-        logo_url: "http://example.com/logos/sriwijaya.png",
-
-    },
-    {
-        code: "NA789",
-        name: "NAM Air",
-        since: "2013",
-        logo_url: "http://example.com/logos/namair.png",
-
-    },
-    {
-        code: "TP890",
-        name: "Trigana Air",
-        since: "1991",
-        logo_url: "http://example.com/logos/trigana.png",
-
-    },
-    {
-        code: "WI901",
-        name: "Wings Air",
-        since: "2003",
-        logo_url: "http://example.com/logos/wingsair.png",
-
-    },
-    {
-        code: "SU012",
-        name: "Super Air Jet",
-        since: "2020",
-        logo_url: "http://example.com/logos/superairjet.png",
-
-    }
-]
+const fs = require("fs")
 
 async function airlaneSeed(prisma) {
     try {
+        const rawData = fs.readFileSync('./assets/core/airlines_seed_data.json');
+        const data = JSON.parse(rawData);
         await prisma.airlines.createMany({ data })
         console.log('airlines data seeded successfully');
     } catch (error) {
--- prisma/seeder/continent_seed.js
+++ prisma/seeder/continent_seed.js
@@ -1,32 +1,9 @@
-const data = [
-    {
-        name: 'Asia',
-        code: 'AS'
-    },
-    {
-        name: 'Amerika',
-        code: 'AM'
-    },
-    {
-        name: 'Eropa',
-        code: 'EU'
-    },
-    {
-        name: 'Afrika',
-        code: 'AF'
-    },
-    {
-        name: 'Australia',
-        code: 'AU'
-    },
-    {
-        name: 'Antarctica',
-        code: 'AN'
-    }
-];
+const fs = require("fs")
 
 async function continentSeed(prisma) {
     try {
+        const rawData = fs.readFileSync('./assets/core/continent_seed_data.json');
+        const data = JSON.parse(rawData);
         await prisma.continent.createMany({ data })
         console.log("continent data seeded succesfully")
     } catch (error) {
