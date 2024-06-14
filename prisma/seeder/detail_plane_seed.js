ba2cb3afb69e9a0a1c0118bacdebd2d1c453e0ae
Akhdanre <danakhdan12@gmail.com>
2024-06-15 00:19:40 +0700

    fix : seeder

--- prisma/seeder/detail_plane_seed.js
+++ prisma/seeder/detail_plane_seed.js
@@ -2,8 +2,38 @@ const fs = require("fs")
 
 async function detailPlaneSeed(prisma) {
     try {
-        const rawData = fs.readFileSync('./assets/core/detail_plane_seed_data.json');
-        const data = JSON.parse(rawData);
+        // const rawData = fs.readFileSync('./assets/core/detail_plane_seed_data.json');
+        // const data = JSON.parse(rawData);
+        const data = []
+
+        for (let i = 1; i < 25; i++) {
+            data.push({
+                total_seat: 120,
+                seat_class_id: 1,
+                plane_id: i
+            })
+            data.push({
+                total_seat: 75,
+                seat_class_id: 2,
+                plane_id: i
+            })
+            data.push({
+                total_seat: 60,
+                seat_class_id: 3,
+                plane_id: i
+            })
+            data.push({
+                total_seat: 30,
+                seat_class_id: 4,
+                plane_id: i
+            })
+            data.push({
+                total_seat: 15,
+                seat_class_id: 5,
+                plane_id: i
+            })
+
+        }
         await prisma.detailPlane.createMany({ data })
         console.log('detailPlane data seeded successfully');
     } catch (error) {
