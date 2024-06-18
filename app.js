e2438f2926b9b6a7e0a2c9d31740f45bd7db7949
Akhdanre <danakhdan12@gmail.com>
2024-06-18 21:35:21 +0700

    feat : error handler middlware and add sentry

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 require("dotenv").config();
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cors = require("cors");
 const routes = require("./routes");
 const task = require("./service/cron_schedule_service");
+const errorMiddleware = require("./middlewares/error_middleware");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+
 
 app.use(cors());
 app.use(logger('dev'));
@@ -23,24 +40,9 @@ app.get("/", (req, res) => {
 
 task.start()
 
-// 404 error handler
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: false,
-    message: `are you lost? ${req.method} ${req.url} is not registered!`,
-    data: null,
-  });
-});
+app.use(errorMiddleware);
 
-// 500 error handler
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({
-    status: false,
-    message: err.message,
-    data: null,
-  });
-});
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => console.log("Listening on port :", PORT));
 
