973a8ef0874ae9dca1a11924093d094894984fd0
Akhdanre <danakhdan12@gmail.com>
2024-06-30 15:25:30 +0700

    fix: condition when req query null

--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -3,11 +3,27 @@ const fs = require('fs');
 const getRandomImage = require('../libs/image_access');
 
 const getImageByCity = async (req, res) => {
-    const city = req.query.city.toLowerCase();
-    const id = req.query.city.toLowerCase();
+    const city = req.query.city ? req.query.city.toLowerCase() : null;
+    const id = req.query.id ? req.query.id.toLowerCase() : null; 
+
+    if (!city) {
+        const defaultImagePath = path.join(__dirname, '../public/images/destination/default.jpeg');
+        return res.sendFile(defaultImagePath);
+    }
+
     try {
         const imagePath = path.join(__dirname, '../public/images/destination', city);
         await fs.promises.access(imagePath, fs.constants.F_OK);
+
+        if (id) {
+            const specificImagePath = path.join(imagePath, `${id}.jpeg`);
+            try {
+                await fs.promises.access(specificImagePath, fs.constants.F_OK);
+                return res.sendFile(specificImagePath);
+            } catch (err) {
+                console.log(`File dengan ID ${id} tidak ditemukan, mengambil gambar acak.`);
+            }
+        }
 
         const randomImage = await getRandomImage(imagePath);
         res.sendFile(randomImage);
