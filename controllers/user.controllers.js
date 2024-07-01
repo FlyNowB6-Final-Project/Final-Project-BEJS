d97c7eb68111e500cdb31e5de17f7b6727609825
Bagus Dwi <sugab.dwi88@gmail.com>
2024-07-01 15:01:34 +0700

    Google

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET_KEY, FRONT_END_URL, URL_RESET_PASS } = process.env;
 const { generatedOTP } = require("../utils/otpGenerator");
 const nodemailer = require("../utils/nodemailer");
+const axios = require('axios');
 const { formatDateToUTC, formatDateTimeToUTC } = require("../utils/formattedDate");
 // const { formattedDate } = require("../utils/formattedDate");
 
@@ -395,5 +396,69 @@ module.exports = {
     const redirectUrl = `${FRONT_END_URL}/?token=${token}`;
 
     return res.redirect(redirectUrl);
+  },
+  LoginGoogle: async (req, res, next) => {
+    try {
+      // Destructures 'access_token' from the request body
+      const { access_token } = req.body;
+
+      if (!access_token) {
+        return res.status(400).json({
+          status: false,
+          message: 'Missing required field',
+          data: null,
+        });
+      }
+
+      // Gets Google user data using the access token
+      const googleData = await axios.get(
+        `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+      );
+
+      // Extracts the full name and family name from the Google data
+      const fullName = googleData?.data?.name;
+      const nameParts = fullName.split(' ');
+      const familyName = nameParts.length > 1 ? nameParts.pop() : '';
+      const firstName = nameParts.join(' ');
+
+      // Upserts user data in case the user already exists in the database
+      const user = await prisma.user.upsert({
+        where: {
+          email: googleData?.data?.email, // Uses the email from the Google data as a unique identifier
+        },
+        update: {
+          fullname: firstName, // Updates the user's full name if they already exist
+          family_name: familyName, // Updates the user's family name if they already exist
+          google_id: googleData?.data?.sub, // Updates the user's Google ID
+          isVerified: true, // Ensures the email is marked as verified
+        },
+        create: {
+          email: googleData?.data?.email,
+          fullname: firstName,
+          family_name: familyName,
+          password: '',
+          isVerified: true,
+          google_id: googleData?.data?.sub,
+        },
+      });
+
+      // Deletes the user's password from the user object for security reasons
+      delete user.password;
+
+      // Creates a JWT token for the user
+      const token = jwt.sign(user, JWT_SECRET_KEY);
+
+      // Returns a successful response with the user data and token
+      return res.status(200).json({
+        status: true,
+        message: 'Successfully login with Google',
+        data: {
+          user,
+          token,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   },
 };
